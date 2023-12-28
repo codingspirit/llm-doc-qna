426741c431288581bd124eabffe3818c3e86549a
Aaron Tsui <aaron.tsui@outlook.com>
2023-12-28 14:35:01 +0800

    UnstructuredMarkdownLoader for README.md

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@
 # @date 2023-12-27
 # @copyright Copyright (c) 2023
 
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import TextLoader, DirectoryLoader, UnstructuredMarkdownLoader
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.embeddings import OpenAIEmbeddings
@@ -17,23 +17,26 @@ re_embedding = True
 
 embeddings = OpenAIEmbeddings()
 
-doc_path = input("Please input doc path \n")
-base_path = os.path.dirname(doc_path)
-db_name = Path(doc_path).stem
+dir_path = input("Please input markdown doc's dir path \n")
+# base_path = os.path.dirname(dir_path)
+db_name = Path(dir_path).stem
 
 
-if not os.path.exists(base_path + db_name):
+if not os.path.exists(db_name):
     print("No existing VDB found. Reading the doc...")
-    loader = TextLoader(doc_path)
+    # loader = TextLoader(doc_path)
+    # loader = DirectoryLoader(base_path, glob="**/*.md")
+    # loader = DirectoryLoader(base_path, glob="README.md", loader_cls=TextLoader)
+    loader = UnstructuredMarkdownLoader(dir_path + "/README.md")
     documents = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.split_documents(documents)
     db = FAISS.from_documents(texts, embeddings)
-    db.save_local(base_path + db_name)
+    db.save_local(db_name)
     print("Finished")
 else:
     print("Existing VDB found.")
-    db = FAISS.load_local(base_path + db_name, embeddings)
+    db = FAISS.load_local(db_name, embeddings)
 
 qa = RetrievalQA.from_chain_type(
     llm=ChatOpenAI(temperature = 0.0), chain_type="stuff", retriever=db.as_retriever()
