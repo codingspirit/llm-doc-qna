fc15d616388253b22b04e1f2cbac2cfe05d37ab1
zhiqinli <zhiqinli@amazon.com>
2024-01-05 13:35:12 +0800

    add streaming ouputs
    Signed-off-by: zhiqinli <zhiqinli@amazon.com>

--- agent.py
+++ agent.py
@@ -9,39 +9,45 @@ from langchain.chains import RetrievalQA
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 from pathlib import Path
 import os
-
-re_embedding = True
+import sys
 
 embeddings = OpenAIEmbeddings()
 
 doc_path = input("Please input doc path \n")
-base_path = os.path.dirname(doc_path)
-db_name = Path(doc_path).stem
+db_name = Path(doc_path).stem + "_vdb"
+db_path = sys.path[0] + "/" + db_name
 
-
-if not os.path.exists(base_path + db_name):
+if not os.path.exists(db_path):
     print("No existing VDB found. Reading the doc...")
     loader = TextLoader(doc_path)
     documents = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = CharacterTextSplitter(chunk_size=1024, chunk_overlap=0)
     texts = text_splitter.split_documents(documents)
     db = FAISS.from_documents(texts, embeddings)
-    db.save_local(base_path + db_name)
+    db.save_local(db_path)
     print("Finished")
 else:
     print("Existing VDB found.")
-    db = FAISS.load_local(base_path + db_name, embeddings)
+    db = FAISS.load_local(db_path, embeddings)
+
+llm = ChatOpenAI(
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
+    temperature=0,
+)
 
 qa = RetrievalQA.from_chain_type(
-    llm=ChatOpenAI(), chain_type="stuff", retriever=db.as_retriever()
+    llm=llm, chain_type="stuff", retriever=db.as_retriever()
 )
 
 while True:
-    query = input('Type your query, or type "exit" to terminate: \n')
+    query = input('\nType your query, or type "exit" to terminate: \n')
     if query == "exit":
         break
-    result = qa.run(query)
-    print(result)
+    print("\nthinking...\n")
+    qa.run(query)
+    print("\n")
